ce8bb8691571277a138d44b90995091674c7cf11
Demonic8 <159974672+Demonic8@users.noreply.github.com>
2024-02-21 10:31:19 +0800

    Nav View

--- app/src/main/java/com/example/e_library/Home.java
+++ app/src/main/java/com/example/e_library/Home.java
@@ -1,18 +1,24 @@
 package com.example.e_library;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Home extends AppCompatActivity {
-FirebaseAuth auth;
+    private DrawerLayout drawerLayout;
+    private ImageButton navButton;
+
+    FirebaseAuth auth;
 Button button;
 TextView textView;
 FirebaseUser user;
@@ -23,8 +29,20 @@ FirebaseUser user;
 
         auth = FirebaseAuth.getInstance();
         button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navButton = findViewById(R.id.navigation_button);
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
         if (user == null){
             Intent intent = new Intent(getApplicationContext(), login.class);
             startActivity(intent);
@@ -42,5 +60,13 @@ FirebaseUser user;
                 finish();
             }
         });
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
